dd76e94b6c84d981249f4c1a475046f15252f35f
Aziz01dev <azizmominov0023@gmail.com>
2025-03-01 19:27:36 +0500

    yangi liniya qoshdim

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
